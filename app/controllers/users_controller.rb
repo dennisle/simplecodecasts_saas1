c6ed695ce49ea2d453bb266498affc44597fb6a8
Dennis Le <ledennis01@gmail.com>
2015-07-22 17:17:59 +0000

    Made corrections to index and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
 def index
+  @users = User.includes(:profile)
 end
 
   def show
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
+  
   belongs_to :user
    
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
